30bceaeda4cd55c452b5d6122e6bac7e852c6b77
JrEmanuel <90966119+JrEmanuel@users.noreply.github.com>
2022-09-08 09:32:46 -0400

    Delete src directory

--- src/events/error.js
+++
@@ -1,3 +0,0 @@
-module.exports = async (bot, err) => {
-    console.log(`${require("../modules/utils").errorPrefix}${err}`);
-};
--- src/events/inviteCreate.js
+++
@@ -1,6 +0,0 @@
-const Utils = require("../modules/utils");
-
-module.exports = async (bot, invite) => {
-    if (Utils.variables.config.Other.IgnoredGuilds.includes(invite.guild.id)) return;
-    Utils.updateInviteCache(bot);
-};
